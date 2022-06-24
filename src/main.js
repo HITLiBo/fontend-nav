259d51242fc4798c1816ce2be02bf06abec833db
MuLi <1140537275@qq.com>
2022-06-24 16:36:37 +0800

    前端导航初版

--- src/main.js
+++ src/main.js
@@ -1,21 +1,60 @@
-$('.addButton').on('click', () => {
-    let url = window.prompt('请问你要添加的网址是?')
-    let newurl;
-    let Newurl;
-    if (url.indexOf('http') !== 0) {
-        newurl = 'https://' + url;
-    }
-    console.log(newurl)
-    const $siteList = $('.siteList')
-    const $lastList = $siteList.find('li.last')
-    Newurl = url.toUpperCase();
-    const $li = $(` <li>
-                <a href="${newurl}">
-                    <div class="site">
+const $siteList = $(".siteList");
+const $lastList = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  {
+    logo: "A",
+    url: "https://www.acfun.cn",
+  },
+  {
+    logo: "B",
+    url: "https://www.bilibili.com",
+  },
+];
 
-                        <div class="logo">${Newurl[0]}</div>
-                        <div class="link">${url}</div>
+const simplify = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, "");
+};
+
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(` <li>
+               
+                    <div class="site">
+                        <div class="logo">${node.logo[0]}</div>
+                        <div class="link">${simplify(node.url)}</div>
+                        <div class="close"><img src="img/close.png" alt="" class='icon'></div>
                     </div>
-                </a>
-            </li>`).insertBefore($lastList)
-})
+                
+            </li>`).insertBefore($lastList);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation();
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+render();
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要添加的网址是?");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  hashMap.push({ logo: simplify(url)[0].toUpperCase(), url: url });
+
+  render();
+});
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  window.localStorage.setItem("x", string);
+};
